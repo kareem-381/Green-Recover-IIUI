6fd2318742da14e2d03e9a2b907c8fca68797166
Abdul Kareem <abdulkareem7646767@gmail.com>
2026-06-19 09:28:31 +0500

    Added Meta Tags

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,6 +1,19 @@
 // src/app/layout.js
 import "./globals.css";
 
+
+export const metadata = {
+  title: "Green Recover | IIUI Lost & Found Portal",
+  description: "The official centralized lost and found web utility portal for the International Islamic University Islamabad community.",
+  keywords: ["IIUI", "Green Recover", "Lost and Found IIUI", "Islamic University lost and found Porta", "IIUI Lost & Found"],
+  openGraph: {
+    title: "Green Recover IIUI",
+    description: "Report missing items or broadcast found objects safely within the campus network.",
+    url: "https://green-recover-iiui.vercel.app/Home",
+    type: "website",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
